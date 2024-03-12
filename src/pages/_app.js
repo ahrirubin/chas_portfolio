41a0b9f173e2ff3f6082730b77fbe0b6bae15d50
ahrirubin <ahri.rubin@gmail.com>
2024-03-12 14:26:30 +0100

    pushing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const { state } = useTheme();
+
+  useEffect(() => {
+    const htmlTag = document.getElementsByTagName("html");
+    console.log(state.theme + " this is theme state");
+
+    if (state.theme) {
+      htmlTag.classlist.add("dark");
+    }
+  }, [state.theme]);
+
   return <Component {...pageProps} />;
 }
+
+export default ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <MyApp Component={Component} pageProps={pageProps} />
+  </ThemeProvider>
+);
